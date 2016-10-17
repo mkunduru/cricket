7805a0c4aa05729c884ca1d73b40b567817abd19
Drew Powers <drew@pow.rs>
2016-10-17 11:57:47 -0400

    Add slick slider

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,31 @@
-//= require_tree .
+/**
+ * Cricket
+ *
+ * Slider JavaScript
+ */
+
+/**
+ * @section Modules
+ */
+
+//= require 'jquery-3.1.1.min'
+//= require 'slick.min'
+
+/**
+ * @section Slider
+ */
+
+$(document).ready(function() {
+  $('.slider .slider-slides').slick({
+    nextArrow: $('.slider-nav-next'),
+    prevArrow: $('.slider-nav-prev'),
+    swipeToSlide: true
+  });
+  $('.slider-nav-next, .slider-nav-prev').click(function(e) {
+    var $this = $(this);
+    $this.addClass('is-sliding');
+    setTimeout(function() {
+      $this.removeClass('is-sliding');
+    }, 200);
+  });
+});
